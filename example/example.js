8af7c1aa0b08151d880bbfba497306f4c6946a0f
jupe <jussiva@gmail.com>
2013-05-17 11:26:26 +0300

    Update example.js

--- example/example.js
+++ example/example.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-var chai = require('chai');
-
 
 mongoose.connect('mongodb://localhost/test');
 var testSchema = new mongoose.Schema({
@@ -10,22 +8,35 @@ var testSchema = new mongoose.Schema({
 var model = mongoose.model('test', testSchema);
 
 var index=0;
-setInterval( function(){
-  var doc = new model({str: 'test#'+index});
-  doc.save();
-  index++;
+
+var i = setInterval( function(){
+  for(var i=0;i<3;i++){
+    var doc = new model({str: 'test#'+index});
+    doc.save();
+    index++;
+  }
 }, 4000);
 
-
 var mongooseTail = require('../lib/');
-var tail = new mongooseTail.Tail({timefield: 'timestamp', modelname: 'test', start: true, select: 'str'});
+var tail = new mongooseTail.Tail({
+                timefield: 'timestamp', 
+                modelname: 'test', 
+                start: true, select: 'str',
+                timeOffset: 10,
+                olderThan: true,
+                limit: 1,
+                count: false,
+                });
 
 tail.on('tick', function(conditions){
   //console.log('tick');
+});
+tail.on('count', function(count){
+  console.log('New count: '+count);
 });
 tail.on('error', function(error){
   console.log('error');
 });
 tail.on('data', function(data){
-  console.log('New data: '+JSON.stringify(data));
+  console.log('New data: '+data);
 });
